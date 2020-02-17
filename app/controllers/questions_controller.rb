c3997af8349ace38b9c8fd96353dcfbc53d08a0a
Ubuntu <ubuntu@ip-172-31-35-219.us-east-2.compute.internal>
2020-02-17 12:59:19 +0000

    Adição de before_each ao método answer_question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :set_question, only: [:show, :update, :destroy]
+  before_action :set_question, only: [:show, :update, :destroy, :answer_question]
 
   # GET /questions
   def index

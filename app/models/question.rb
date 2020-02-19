a1290a8266256889cca877e016f073aca704be7b
Ubuntu <ubuntu@ip-172-31-35-219.us-east-2.compute.internal>
2020-02-19 19:11:02 +0000

    Correção no Enum de dificuldade da pergunta

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,6 +3,6 @@ class Question < ApplicationRecord
   has_many :game_questions, dependent: :destroy
   belongs_to :correct_answer, foreign_key: "correct_answer_id", class_name: "Answer", optional: true
   
-  enum _level: [ :FACIL, :MEDIO, :DIFICIL, :MUITO_DIFICIL ]
+  enum level: [ :FACIL, :MEDIO, :DIFICIL, :MUITO_DIFICIL ]
   
 end
